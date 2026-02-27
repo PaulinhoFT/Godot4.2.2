673b190b147173a5522b8d934e6082e1691849a9
PaulinhoFT <pfelix452@gmail.com>
2026-02-27 16:15:03 -0300

    Update primeiroscript.gd

--- primeiroscript.gd
+++ primeiroscript.gd
@@ -1,10 +1,26 @@
 extends Node2D
 
+#var vidas = 5
+var tempoRestante = 64.5
+var nomeFase = "Fase da Agua"
+var powerUp = false
+
+#VARIAVEL
+var vidas: int = 5
+#CONSTANTE
+const VIDAS_PLAYER = 5
+
 func _ready():
-	print("Hello, World!")
-	
-	
-	#pass 
+	print("Hello, World! " + str(5+6))
+	print(vidas)
+	print(tempoRestante)
+	print(nomeFase)
+	print(powerUp)
+	powerUp = true
+	print(powerUp)
+	vidas = vidas + 1
+	print(vidas)
+	pass 
 	
 """
 func _process(delta):
